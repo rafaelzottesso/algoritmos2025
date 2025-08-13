44374d57b98bb8557d8554f6dd2b3cd3bf2dc03d
Professor <rafael.zottesso@ifpr.edu.br>
2025-08-12 22:33:20 -0300

    Procedimentos e funções: criação, importação e uso

---
+++ Códigos de aula/08-12 - Procedimentos e funções.py
@@ -0,0 +1,25 @@
+import biblioteca as bib
+#bib.ola()
+
+num1 = bib.inputInt("Digite um número: ")
+num2 = bib.inputInt("Digite outro: ")
+print("os números são: ",num1," ",num2)
+
+res = bib.somar2(num1, num2)
+print(f"{num1} + {num2} = {res}")
+
+raiz = bib.raiz(num1, num2)
+print(f"A raiz de {num1} com grau {num2} é {raiz}")
+
+res = bib.pot(num1, num2)
+print(f"{num1} elevado a {num2} é {res}")
+
+num3 = bib.inputInt("Mais um int: ")
+res = bib.somar3(num1, num2, num3)
+print("A soma dos números é", res)
+
+tipo = bib.ePar(num1)
+if(tipo == True):
+    print(f"{num1} é par")
+else:
+    print(f"{num1} é ímpar")
---
+++ Códigos de aula/biblioteca.py
@@ -0,0 +1,73 @@
+# Função sem retorno e sem parâmetro
+def ola():
+    print("Olá, Bem Vindo!")
+    if(True):
+        print("Função Válida")
+    else:
+        print("Erro")
+
+# Função que recebe uma mensagem do tipo str
+# e retorna um número inteiro
+def inputInt(msg:str):
+    
+    x = input(f"{msg}")
+    
+    # O try/except evitam que o programa apresente um erro
+    # e para a execução. Ele tenta (try) executar o código
+    # e em caso de erro executa a alternativa (except)
+    try:
+        x = int(x)
+    except:
+        print("Int inválido!")
+        # Funções recursivas fazem uma chamada para ela mesmo
+        x = inputInt()
+    return x
+
+# Função sem parâmetro que retorna um valor float
+# Enquanto não for possível converter para float, o programa
+# continua pedindo um valor ao usuário
+def pedirFloat():
+    while(True):
+        try:
+            x = float(input("Numero: "))
+            break
+        except:
+            print("Float inválido")
+    return x
+
+# Funções com parâmetros
+# Não importa o valor deles, mas sim o comportamento da função
+# A função não tem a obrigação de saber os valores, deve apenas calcular o que for necessário
+# Podemos especificar o tipo do parâmetro que será recebido
+# nomeDoParametro:tipoDele
+# Ao separar por vírgula, podemos aumentar a quantidade de parâmetros recebidos
+def somar2(x:int, y:int):
+    total = x + y
+    return total
+
+# Aqui temos três e não importa o tipo deles
+def somar3(v1,v2,v3):
+    soma = v1 + v2 + v3
+    return soma
+
+# Função com dois parâmetros
+def pot(valor:int,potencia:int):
+    resultado = valor**potencia
+    return resultado
+
+# Função com dois parâmetros
+def raiz(valor:float,grau:int):
+    resultado = valor**(1/grau)
+    return resultado
+
+# Cria a função chamada ePar que recebe
+# um parametro inteiro e retona:
+# True se o parametro for par
+# False se o parametro for Impar
+def ePar(valor:int):
+    resto = valor % 2
+    if(resto == 0):
+        return True
+    else:
+        return False
+
